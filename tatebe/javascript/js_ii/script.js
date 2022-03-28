45d8477300e9d0ca32f2dbd33f0a40a8c956e261
tatebe kyo <ttb.foxxx47@gmail.com>
2022-03-28 18:10:43 +0900

    17. 総合演習(2)

--- tatebe/javascript/js_ii/script.js
+++ tatebe/javascript/js_ii/script.js
@@ -134,13 +134,31 @@ for (let i = 0; i < characters.length; i++) {
 
 
 // 16. 総合演習(1)
-const cafe = {
-  name: "Progateカフェ",
-  businessHours: {
-    opening: '10:00(AM)',
-    closing: '8:00(PM)'
-  },
-};
+// const cafe = {
+//   name: "Progateカフェ",
+//   businessHours: {
+//     opening: '10:00(AM)',
+//     closing: '8:00(PM)'
+//   },
+// };
 
 console.log(`店名:${cafe.name}`);
 console.log(`営業時間:${cafe.businessHours.opening}から${cafe.businessHours.closing}`);
+
+// 17. 総合演習(2)
+const cafe = {
+  name: "Progateカフェ",
+  businessHours: { 
+    opening: "10:00(AM)",
+    closing: "8:00(PM)"
+  },
+  menus: ['コーヒー', '紅茶', 'チョコレートケーキ']
+};
+
+console.log(`店名: ${cafe.name}`);
+console.log(`営業時間:${cafe.businessHours.opening}から${cafe.businessHours.closing}`);
+console.log(`----------------------------`);
+console.log("おすすめメニューはこちら");
+for (let i = 0; i < cafe.menus.length; i++) {
+  console.log(cafe.menus[i]);  
+}
