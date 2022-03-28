64083fcda065a7c530c479ab666641d53371b125
tatebe kyo <ttb.foxxx47@gmail.com>
2022-03-28 16:22:42 +0900

    16. else if

--- tatebe/javascript/js_i/script.js
+++ tatebe/javascript/js_i/script.js
@@ -84,10 +84,21 @@ if(password !== 'ninjawanko') {
 };
 
 // 15. else
-const age = 17;
+// const age = 17;
 
 if (age >= 20) {
   console.log("私は20歳以上です");
 } else {
   console.log("私は20歳未満です");
+}
+
+// 16. else if
+const age = 17;
+
+if (age >= 20) {
+  console.log("私は20歳以上です");
+} else if(age >= 10) {
+  console.log("私は20歳未満ですが、10歳以上です");
+} else {
+  console.log("私は10歳未満です");
 }
