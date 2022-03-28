6a985ccee7f36f91ecba21868f5177910d860872
tatebe kyo <ttb.foxxx47@gmail.com>
2022-03-28 17:26:02 +0900

    2. while文

--- tatebe/javascript/js_ii/script.js
+++ tatebe/javascript/js_ii/script.js
@@ -1,5 +1,5 @@
 // 1. 変数の復習
-let number = 0;
+// let number = 0;
 number += 1;
 console.log(number);
 
@@ -11,3 +11,11 @@ number += 1;
 console.log(number);
 number += 1;
 console.log(number);
+
+// 2. while文
+let number = 1;
+
+while (number <= 100) {
+  console.log(number);
+  number += 1;
+}
