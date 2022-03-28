d65ff807db8ea77aad7860e849a5d23b38fc1d24
tatebe kyo <ttb.foxxx47@gmail.com>
2022-03-28 12:55:47 +0900

    9. 変数の値の更新（2）

--- tatebe/javascript/js_i/script.js
+++ tatebe/javascript/js_i/script.js
@@ -38,3 +38,16 @@ console.log(name);
 
 name = "とりずきん";
 console.log(name);
+
+
+// 9. 変数の値の更新（2）
+let number = 7;
+console.log(number);
+
+// 変数numberの値に3を加えてください
+number += 3;
+console.log(number);
+
+// 変数numberの値を2で割ってください
+number /= 2;
+console.log(number);
