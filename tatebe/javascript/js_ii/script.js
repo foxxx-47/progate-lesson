48e9350c1928fcfe9c5ebc8a16f6b6be3910ba0b
tatebe kyo <ttb.foxxx47@gmail.com>
2022-03-28 17:24:06 +0900

    1. 変数の復習

---
+++ tatebe/javascript/js_ii/script.js
@@ -0,0 +1,13 @@
+// 1. 変数の復習
+let number = 0;
+number += 1;
+console.log(number);
+
+number += 1;
+console.log(number);
+number += 1;
+console.log(number);
+number += 1;
+console.log(number);
+number += 1;
+console.log(number);
