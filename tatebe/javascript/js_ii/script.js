c127e3d254253b177d1ab4bdedc58a1fe3561cbe
tatebe kyo <ttb.foxxx47@gmail.com>
2022-03-28 17:28:00 +0900

    3. for文

--- tatebe/javascript/js_ii/script.js
+++ tatebe/javascript/js_ii/script.js
@@ -13,9 +13,14 @@ number += 1;
 console.log(number);
 
 // 2. while文
-let number = 1;
+// let number = 1;
 
 while (number <= 100) {
   console.log(number);
   number += 1;
+}
+
+// 3. for文
+for (let number = 1; number <= 100; number++) {
+  console.log(number);
 }
