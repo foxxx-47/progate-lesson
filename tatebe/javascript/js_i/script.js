ed937b8742749e88fa5b074b8eeada628e60978f
tatebe kyo <ttb.foxxx47@gmail.com>
2022-03-28 10:18:16 +0900

    3. 計算してみよう

--- tatebe/javascript/js_i/script.js
+++ tatebe/javascript/js_i/script.js
@@ -1,4 +1,14 @@
+// 1
+console.log("「ES6 学習コース」へようこそ！");
+console.log("にんじゃわんこと一緒にES6を学んでいきましょう。");
+
+// 2
 console.log('Hello World');
 console.log('にんじゃわんこ');
 
 // console.log("この行をコメントアウトしてください");
+
+// 3
+console.log(5 + 3);
+console.log(20 - 8);
+console.log('4 + 5');
