33c550e1bd37b5b628e1c2f7168ad3694b400a44
tatebe kyo <ttb.foxxx47@gmail.com>
2022-03-29 09:52:13 +0900

    2. 関数とは（2）

--- tatebe/javascript/js_iii/script.js
+++ tatebe/javascript/js_iii/script.js
@@ -4,3 +4,13 @@ const greet = function() {
   console.log("関数を学習していきましょう！");
 };
 greet();
+
+// 2. 関数とは（2）
+const hello = function() {
+  console.log("こんにちは！");
+  console.log("私の名前はたてべです");
+}
+hello();
+
+// 3. アロー関数
+
