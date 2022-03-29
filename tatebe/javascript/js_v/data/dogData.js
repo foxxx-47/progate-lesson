35934ce7f1f553832e43e225366fd333aa46ae92
tatebe kyo <ttb.foxxx47@gmail.com>
2022-03-29 11:57:31 +0900

    パッケージ（2）

--- tatebe/javascript/js_v/data/dogData.js
+++ tatebe/javascript/js_v/data/dogData.js
@@ -1,6 +1,11 @@
+import readlineSync from 'readline-sync';
 import Dog from "../class/dog";
 
 const dog1 = new Dog("レオ", 4, "チワワ");
-const dog2 = new Dog("ベン", 2, "プードル");
+const name = readlineSync.question("名前を入力してください: ");
+const age = readlineSync.questionInt("年齢を入力してください: ");
+const breed = readlineSync.question("犬種を入力してください: ");
 
-export {dog1, dog2};
+const dog2 = new Dog(name, age, breed);
+
+export { dog1, dog2 };
