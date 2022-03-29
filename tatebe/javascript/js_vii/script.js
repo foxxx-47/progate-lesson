81ddfb3a50ad3461a90a6ec3d34423e8085d5612
tatebe kyo <ttb.foxxx47@gmail.com>
2022-03-29 14:03:45 +0900

    コールバック関数の引数

--- tatebe/javascript/js_vii/script.js
+++ tatebe/javascript/js_vii/script.js
@@ -1,12 +1,5 @@
-const printWanko = () => {
-  console.log("にんじゃわんこ");
-};
+const call = (callback) => { callback("にんじゃわんこ", 14); };
 
-const call = (callback) => {
-  console.log("コールバック関数を呼び出します。");
-  callback();
-};
-
-call(printWanko);
-
-call(() => {console.log('ひつじ仙人');});
+call((name, age) => {
+  console.log(`${name}は${age}歳です。`);
+});
