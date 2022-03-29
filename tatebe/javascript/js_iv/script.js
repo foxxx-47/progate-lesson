4e6cc69d17664d25506740b040838e90e6c4a5ec
tatebe kyo <ttb.foxxx47@gmail.com>
2022-03-29 10:41:40 +0900

    3. インスタンスの生成

--- tatebe/javascript/js_iv/script.js
+++ tatebe/javascript/js_iv/script.js
@@ -1,14 +1,18 @@
 // 1. オブジェクトの復習
-const animal = {
-  name: 'レオ',
-  age: 3,
-  greet: () => {
-    console.log('こんにちは');
-  },
-};
+// const animal = {
+//   name: 'レオ',
+//   age: 3,
+//   greet: () => {
+//     console.log('こんにちは');
+//   },
+// };
 
 console.log(animal.name);
 animal.greet();
 
 // 2. クラスとは
 class Animal {}
+
+// 3. インスタンスの生成
+const animal = new Animal();
+console.log(animal);
