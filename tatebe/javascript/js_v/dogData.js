a6cf5c32efb6a5a00c8000c2d2d667d0394aaedb
tatebe kyo <ttb.foxxx47@gmail.com>
2022-03-29 11:43:22 +0900

    名前付きエクスポート

--- tatebe/javascript/js_v/dogData.js
+++ tatebe/javascript/js_v/dogData.js
@@ -1,5 +1,6 @@
 import Dog from "./dog";
 
-const dog = new Dog("レオ", 4, "チワワ");
+const dog1 = new Dog("レオ", 4, "チワワ");
+const dog2 = new Dog("ベン", 2, "プードル");
 
-export default dog;
+export {dog1, dog2};
--- tatebe/javascript/js_v/script.js
+++ tatebe/javascript/js_v/script.js
@@ -1,3 +1,6 @@
-import dog from "./dogData";
+import {dog1, dog2} from "./dogData";
 
-dog.info();
+console.log("---------");
+dog1.info();
+console.log("---------");
+dog2.info();
