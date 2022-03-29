69e46d05278720bdbbb11660b0df7bbb0fd6aff4
tatebe kyo <ttb.foxxx47@gmail.com>
2022-03-29 13:44:10 +0900

    filterメソッド

--- tatebe/javascript/js_vi/script.js
+++ tatebe/javascript/js_vi/script.js
@@ -1,16 +1,12 @@
-const numbers = [1, 3, 5, 7, 9];
-const foundNumber = numbers.find((number) => {
-  return number % 3 === 0;
-});
-
-console.log(foundNumber);
+const numbers = [1, 2, 3, 4];
+const evenNumbers = numbers.filter((number) => {return number % 2 === 0});
+console.log(evenNumbers);
 
 const characters = [
-  {id: 1, name: "にんじゃわんこ", age: 6},
-  {id: 2, name: "ベイビーわんこ", age: 2},
-  {id: 3, name: "ひつじ仙人", age: 100},
-  {id: 4, name: "とりずきん", age: 21}
+  {id: 1, name:"にんじゃわんこ", age: 14},
+  {id: 2, name:"ベイビーわんこ", age: 5},
+  {id: 3, name:"ひつじ仙人", age: 100}
 ];
 
-const foundCharacter = characters.find((character) => {return character.id === 3});
-console.log(foundCharacter);
+const underTwenty = characters.filter((character) => {return character.age < 20});
+console.log(underTwenty);
