f787d08df17ce51586b975b193e819cf5c729c20
tatebe kyo <ttb.foxxx47@gmail.com>
2022-03-29 11:22:52 +0900

    オーバーライド（2）

--- tatebe/javascript/js_iv/script.js
+++ tatebe/javascript/js_iv/script.js
@@ -19,6 +19,11 @@ const animal = new Animal('レオ', 3);
 animal.info();
 
 class Dog extends Animal {
+  constructor(name, age, breed) {
+    super(name, age);
+    this.breed = breed;
+  }
+
   getHumanAge() {
     return this.age * 7;
   }
@@ -26,9 +31,10 @@ class Dog extends Animal {
   info() {
     this.greet();
     console.log(`名前は${this.name}です`);
+    console.log(`犬種は${this.breed}です`);
     console.log(`${this.age}歳です`);
     console.log(`人間年齢で${this.getHumanAge()}歳です`);
   }
 }
-const dog = new Dog('レオ', 4);
+const dog = new Dog('レオ', 4, 'チワワ');
 dog.info();
