4ca77526d274820af3daea22beb13f5a7eaa47aa
tatebe kyo <ttb.foxxx47@gmail.com>
2022-03-29 13:50:22 +0900

    コールバック関数を学ぼう

---
+++ tatebe/javascript/js_vii/script.js
@@ -0,0 +1,10 @@
+const printWanko = () => { console.log("にんじゃわんこ"); };
+
+const printHitsuji = () => { console.log("ひつじ仙人"); };
+
+const call = (callback) => {
+  console.log("コールバック関数を呼び出します。");
+  callback();
+};
+
+call(printHitsuji);
