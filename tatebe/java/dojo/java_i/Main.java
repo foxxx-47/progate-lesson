a70c6d66dc191a56576bfaac6514b6bb5cbdef9a
tatebe kyo <ttb.foxxx47@gmail.com>
2022-03-31 14:26:12 +0900

    条件分岐をしよう

--- tatebe/java/dojo/java_i/Main.java
+++ tatebe/java/dojo/java_i/Main.java
@@ -10,8 +10,18 @@ class Main {
 
     System.out.print("名字：");
     String lastName = scanner.next();
+
+    System.out.print("年齢：");
+    int age = scanner.nextInt();
     
     String fullName = firstName + " " + lastName;
     System.out.println("名前は" + fullName + "です");
+
+    System.out.println("年齢は" + age + "歳です");
+    if (age >= 20) {
+      System.out.println("成年者です");
+    } else {
+      System.out.println("未成年者です");
+    }
   }
 }
