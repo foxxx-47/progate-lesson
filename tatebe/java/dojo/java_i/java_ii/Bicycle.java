8231ab900b3193f45d7e05ac3db7ef893625882f
tatebe kyo <ttb.foxxx47@gmail.com>
2022-03-31 16:53:00 +0900

    色を追加する

--- tatebe/java/dojo/java_i/java_ii/Bicycle.java
+++ tatebe/java/dojo/java_i/java_ii/Bicycle.java
@@ -1,12 +1,15 @@
 class Bicycle {
   private String name;
+  private String color;
 
-  Bicycle(String name) {
+  Bicycle(String name, String color) {
     this.name = name;
+    this.color = color;
   }
 
   public void printData() {
     System.out.println("【自転車の情報】");
     System.out.println("名前：" + this.name);
+    System.out.println("色：" + this.color);
   }
 }
