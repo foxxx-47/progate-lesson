7972fdd447a994639f856bbcd98bf8a839bfcd31
tatebe kyo <ttb.foxxx47@gmail.com>
2022-03-31 15:59:01 +0900

    インスタンスメソッド（２）

--- tatebe/java/java_iv/Person.java
+++ tatebe/java/java_iv/Person.java
@@ -1,3 +1,5 @@
+import java.lang.Math;
+
 class Person {
   public String firstName;
   public String lastName;
@@ -12,12 +14,18 @@ class Person {
     this.height = height;
     this.weight = weight;
   }
-  
+
   public String fullName() {
     return this.firstName + " " + this.lastName;
   }
-  
+
   public double bmi() {
     return this.weight / this.height / this.height;
+  }
+  
+  public void printData() {
+    System.out.println("私の名前は" + this.fullName() + "です");
+    System.out.println("年齢は" + this.age + "歳です");
+    System.out.println("BMIは" + Math.round(this.bmi()) + "です");
   }
 }
