51021b5fd92ea38929625abfce7774c3688f44a7
tatebe kyo <ttb.foxxx47@gmail.com>
2022-03-31 16:55:02 +0900

    走行距離を測ろう

--- tatebe/java/dojo/java_i/java_ii/Bicycle.java
+++ tatebe/java/dojo/java_i/java_ii/Bicycle.java
@@ -1,6 +1,7 @@
 class Bicycle {
   private String name;
   private String color;
+  private int distance = 0;
 
   Bicycle(String name, String color) {
     this.name = name;
@@ -11,5 +12,6 @@ class Bicycle {
     System.out.println("【自転車の情報】");
     System.out.println("名前：" + this.name);
     System.out.println("色：" + this.color);
+    System.out.println("走行距離：" + this.distance + "km");
   }
 }
