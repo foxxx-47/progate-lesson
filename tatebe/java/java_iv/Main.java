6d39d53ece48103015ffa505f2d42c11aae0e6bf
tatebe kyo <ttb.foxxx47@gmail.com>
2022-03-31 16:44:49 +0900

    「仕事」を追加しよう

--- tatebe/java/java_iv/Main.java
+++ tatebe/java/java_iv/Main.java
@@ -1,14 +1,14 @@
 class Main {
   public static void main(String[] args) {
-    Person person1 = new Person("Kate", "Jones", 27, 1.6, 50.0);
+    Person person1 = new Person("Kate", "Jones", 27, 1.6, 50.0, "医者");
     person1.printData();
-    Person person2 = new Person("John", "Christopher", "Smith", 65, 1.75, 80.0);
-    person2.printData();
-
-    System.out.println("----------------------");
-    person1.setMiddleName("Claire");
     
-    System.out.println("ミドルネームを" + person1.getMiddleName() + "に変更しました");
+    Person person2 = new Person("John", "Christopher", "Smith", 65, 1.75, 80.0, "教師");
+    person2.printData();
+    System.out.println("----------------------");
+    person1.setJob("獣医");
+    System.out.println("person1の仕事を" + person1.getJob() + "に変更しました");
+    
     person1.printData();
   }
 }
--- tatebe/java/java_iv/Person.java
+++ tatebe/java/java_iv/Person.java
@@ -1,32 +1,43 @@
 class Person {
   private static int count = 0;
+  
   private String firstName;
   private String middleName;
   private String lastName;
   private int age;
   private double height;
   private double weight;
-
-  Person(String firstName, String lastName, int age, double height, double weight) {
+  private String job;
+  
+  Person(String firstName, String lastName, int age, double height, double weight, String job) {
     Person.count++;
     this.firstName = firstName;
     this.lastName = lastName;
     this.age = age;
     this.height = height;
     this.weight = weight;
+    this.job = job;
   }
-
-  Person(String firstName, String middleName, String lastName, int age, double height, double weight) {
-    this(firstName, lastName, age, height, weight);
+  
+  Person(String firstName, String middleName, String lastName, int age, double height, double weight, String job) {
+    this(firstName, lastName, age, height, weight, job);
     this.middleName = middleName;
   }
 
   public String getMiddleName() {
     return this.middleName;
   }
+  
+  public String getJob() {
+    return this.job;
+  }
 
   public void setMiddleName(String middleName) {
     this.middleName = middleName;
+  }
+  
+  public void setJob(String job) {
+    this.job = job;
   }
 
   public String fullName() {
@@ -41,6 +52,7 @@ class Person {
     System.out.println("私の名前は" + this.fullName() + "です");
     System.out.println("年齢は" + this.age + "歳です");
     System.out.println("BMIは" + Math.round(this.bmi()) + "です");
+    System.out.println("仕事は" + this.job + "です");
   }
 
   public double bmi() {
