2883ba7866dca3fce15123629fa490a13f204378
tatebe kyo <ttb.foxxx47@gmail.com>
2022-03-31 15:25:31 +0900

    最高年齢を求めよう

--- tatebe/java/dojo/java_i/Main.java
+++ tatebe/java/dojo/java_i/Main.java
@@ -7,29 +7,32 @@ class Main {
     System.out.print("何人分の情報を入力しますか：");
     int num = scanner.nextInt();
 
+    int maxAge = 0;
     for(int i = 1; i <= num; i++) {
       System.out.println(i + "人目");
-      personInfo(scanner);
+      System.out.println("名前：");
+      String firstName = scanner.next();
+      System.out.println("名前は" + firstName + "です");
+
+      System.out.print("名字：");
+      String lastName = scanner.next();
+
+      System.out.print("年齢：");
+      int age = scanner.nextInt();
+
+      System.out.print("身長：");
+      double height = scanner.nextDouble();
+
+      System.out.print("体重：");
+      double weight = scanner.nextDouble();
+
+      Person.printData(Person.fullName(firstName, lastName), age, height, weight, Person.bmi(height, weight));
+
+      if (maxAge < age) {
+        maxAge = age;
+      }
     }
-  }
 
-  public static void personInfo(Scanner scanner) {
-    System.out.println("名前：");
-    String firstName = scanner.next();
-    System.out.println("名前は" + firstName + "です");
-
-    System.out.print("名字：");
-    String lastName = scanner.next();
-
-    System.out.print("年齢：");
-    int age = scanner.nextInt();
-
-    System.out.print("身長：");
-    double height = scanner.nextDouble();
-
-    System.out.print("体重：");
-    double weight = scanner.nextDouble();
-
-    Person.printData(Person.fullName(firstName, lastName), age, height, weight, Person.bmi(height, weight));
+    System.out.println("最高年齢は" + maxAge + "歳です");
   }
 }
