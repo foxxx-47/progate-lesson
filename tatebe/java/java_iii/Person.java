7d9bb6c58dca82565d0fc122d3513cf2259f2266
tatebe kyo <ttb.foxxx47@gmail.com>
2022-03-31 13:49:32 +0900

    ライブラリを使ってみよう

--- tatebe/java/java_iii/Person.java
+++ tatebe/java/java_iii/Person.java
@@ -1,12 +1,15 @@
+// java.lang.Mathを読み込んでください
+import java.lang.Math;
+
 class Person {
   public static void printData(String name, int age, double height, double weight) {
     System.out.println("私の名前は" + name + "です");
     System.out.println("年齢は" + age + "歳です");
     System.out.println("身長は" + height + "mです");
     System.out.println("体重は" + weight + "kgです");
-
     double bmi = bmi(height, weight);
-    System.out.println("BMIは" + bmi + "です");
+    // Mathクラスのroundメソッドを用いて、BMIを四捨五入して出力するようにしてください
+    System.out.println("BMIは" + Math.round(bmi) + "です");
 
     if (isHealthy(bmi)) {
       System.out.println("標準値です");
@@ -26,7 +29,7 @@ class Person {
   public static double bmi(double height, double weight) {
     return weight / height / height;
   }
-
+  
   public static boolean isHealthy(double bmi) {
     return bmi >= 18.5 && bmi < 25.0;
   }
