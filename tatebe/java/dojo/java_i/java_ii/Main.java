b07b46450d680ebb413324f57adfb8751bded368
tatebe kyo <ttb.foxxx47@gmail.com>
2022-03-31 16:59:32 +0900

    走る距離を受け取ろう

--- tatebe/java/dojo/java_i/java_ii/Main.java
+++ tatebe/java/dojo/java_i/java_ii/Main.java
@@ -1,10 +1,16 @@
+import java.util.Scanner;
+
 class Main {
   public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
     Bicycle bicycle1 = new Bicycle("ビアンキ", "緑");
     bicycle1.printData();
 
     System.out.println("-----------------");
 
-    bicycle1.run(10);
+    System.out.print("走る距離を入力してください：");
+    int distance = sc.nextInt();
+    bicycle1.run(distance);
   }
 }
