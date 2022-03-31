e3e7a33596c6239769715456032d44aef0358311
tatebe kyo <ttb.foxxx47@gmail.com>
2022-03-31 16:20:23 +0900

    コンストラクタのオーバーロード

--- tatebe/java/java_iv/Person.java
+++ tatebe/java/java_iv/Person.java
@@ -1,14 +1,25 @@
 class Person {
   public static int count = 0;
 
-  public String firstName, lastName;
+  public String firstName;
+  public String middleName;
+  public String lastName;
   public int age;
   public double height, weight;
 
   Person(String firstName, String lastName, int age, double height, double weight) {
     Person.count++;
-    
     this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+    this.height = height;
+    this.weight = weight;
+  }
+  
+  Person(String firstName, String middleName, String lastName, int age, double height, double weight) {
+    Person.count++;
+    this.firstName = firstName;
+    this.middleName = middleName;
     this.lastName = lastName;
     this.age = age;
     this.height = height;
