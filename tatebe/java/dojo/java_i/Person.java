f5430aa0ba9766c7cac623ad2381984956621fab
tatebe kyo <ttb.foxxx47@gmail.com>
2022-03-31 14:59:36 +0900

    論理演算子を使おう

--- tatebe/java/dojo/java_i/Person.java
+++ tatebe/java/dojo/java_i/Person.java
@@ -5,7 +5,7 @@ class Person {
     System.out.println("名前は" + fullName + "です");
   
     System.out.println("年齢は" + age + "歳です");
-    if (age >= 20) {
+    if (isAdult(age)) {
       System.out.println("成年者です");
     } else {
       System.out.println("未成年者です");
@@ -16,13 +16,27 @@ class Person {
     System.out.println("体重は" + weight + "kgです");
 
     System.out.println("BMIは" + Math.round(bmi) + "です");
+
+    if (isHealthy(bmi)) {
+      System.out.println("健康です");
+    } else {
+      System.out.println("健康ではありません");
+    }
   }
 
   public static String fullName(String firstName, String lastName) {
     return firstName + " " + lastName;
   }
+  
+  public static boolean isAdult(int age) {
+    return age >= 20;
+  }
 
   public static double bmi(double height, double weight) {
     return weight / height / height;
+  }
+
+  public static boolean isHealthy(double bmi) {
+    return bmi < 25 && bmi >= 18.5;
   }
 }
