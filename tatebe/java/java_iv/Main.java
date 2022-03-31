1588c0bd368ebebb6eae1b368a122a297e3c2109
tatebe kyo <ttb.foxxx47@gmail.com>
2022-03-31 15:55:01 +0900

    目標物を確認しよう

--- tatebe/java/java_iv/Main.java
+++ tatebe/java/java_iv/Main.java
@@ -1,12 +1,11 @@
 class Main {
   public static void main(String[] args) {
-    Person person1 = new Person("Kate Jones");
-    person1.hello();
-    System.out.println(person1.name);
-
-    Person person2 = new Person("John Christopher Smith");
-    person2.hello();
-    System.out.println(person2.name);
+    Person person1 = new Person("Kate", "Jones", 27, 1.6, 50.0);
     
+    System.out.println(person1.firstName);
+    System.out.println(person1.lastName);
+    System.out.println(person1.age);
+    System.out.println(person1.height);
+    System.out.println(person1.weight);
   }
 }
--- tatebe/java/java_iv/Person.java
+++ tatebe/java/java_iv/Person.java
@@ -1,14 +1,15 @@
 class Person {
-  public String name;
+  public String firstName;
+  public String lastName;
+  public int age;
+  public double height;
+  public double weight;
 
-  // コンストラクタがString型の引数を受け取るようにしてください
-  Person(String name) {
-    System.out.println("インスタンスが生成されました");
-    // インスタンスフィールドnameに値をセットしてください
-    this.name = name;
-  }
-
-  public void hello() {
-    System.out.println("こんにちは、私は" + this.name + "です");
+  Person(String firstName, String lastName, int age, double height, double weight) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+    this.height = height;
+    this.weight = weight;
   }
 }
